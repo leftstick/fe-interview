dd682f361ebc72fef08e36ae86bb2c2ec86f9fa1
Howard.Zuo <leftstick@qq.com>
2015-06-18 15:03:39 +0800

    add currying question

--- bin/questions/currying/Test.js
+++ bin/questions/currying/Test.js
@@ -7,10 +7,32 @@ describe('duplicate', function() {
     var path = require('path');
     var currying = require(path.resolve(process.cwd(), 'currying', 'index'));
 
-    it('basic test', function() {
-        var curried = currying(function(a, b) {
+    it('curries the function at least once', function() {
+        var add = currying(function(a, b) {
             return a + b;
         });
-        assert.equal(curried(10)(29), 39);
+        assert.equal(add(1)(2), 3, '一次柯里化验证失败');
+    });
+
+    it('curries the function until the arguments needed are given at least once', function() {
+        var add = currying(function(a, b, c) {
+            return a + b + c;
+        });
+        assert.equal(add(1, 2)(3), 6, '多参数一次柯里化验证失败');
+    });
+
+    it('curries the function until the arguments needed are given mutliple times', function() {
+        var add = currying(function(a, b, c) {
+            return a + b + c;
+        });
+        assert.equal(add(1)(2)(3), 6, '多参数多次柯里化验证失败');
+    });
+
+    it('doesn\'t share state between calls', function() {
+        var add = currying(function(a, b, c) {
+            return a + b + c;
+        });
+        assert.equal(add(1)(2)(3), 6, '多参数多次柯里化验证失败');
+        assert.equal(add(2)(3)(4), 9, '多参数多次柯里化状态分离验证失败');
     });
 });
