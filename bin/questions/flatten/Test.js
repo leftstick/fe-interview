27a3dd8f37e6a728218a395cd0a94cdbded67942
Howard.Zuo <leftstick@qq.com>
2016-01-08 10:41:42 +0800

    add ut for flatten

--- bin/questions/flatten/Test.js
+++ bin/questions/flatten/Test.js
@@ -9,7 +9,39 @@ describe('flatten', function() {
     var flatten = require(path.resolve(process.cwd(), 'flatten', 'index'));
 
     it('basic test', function() {
-        var arr = [1, [2, [3, 4], 5], 6];
+        var arr = [
+            1,
+            [
+                2,
+                [
+                    3,
+                    4
+                ],
+                5
+            ],
+            6
+        ];
+        assert.deepEqual(_.flatten(arr, true), flatten(arr));
+    });
+
+    it('object array', function() {
+        var arr = [
+            1,
+            [
+                {
+                    name: 'BeiJing'
+                },
+                [
+                    3,
+                    'ZhongGuo'
+                ],
+                {
+                    age: 90,
+                    isMale: false
+                }
+            ],
+            6
+        ];
         assert.deepEqual(_.flatten(arr, true), flatten(arr));
     });
 });
