8fa6a713d8d56ded3ccfb3769faf3c835bb912c0
Howard.Zuo <leftstick@qq.com>
2016-06-28 17:30:38 +0800

    update answer

--- bin/questions/03_currying/answer.js
+++ bin/questions/03_currying/answer.js
@@ -19,10 +19,9 @@ var currying = function(func) {
 
     var getCurry = function(params) {
         return function() {
-            var _this = this;
             var next = params.concat(Array.prototype.slice.call(arguments));
-            if (len - next.length === 0) {
-                return func.apply(_this, next);
+            if (len - next.length <= 0) {
+                return func.apply(this, next);
             }
             return getCurry(next);
         };
